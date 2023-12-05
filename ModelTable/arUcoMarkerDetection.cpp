108c93038e129205602f699e07af83be1cd6e048
lucie <weber_lucie@web.de>
2023-12-05 16:14:24 +0100

    update c++ files

--- ModelTable/arUcoMarkerDetection.cpp
+++ ModelTable/arUcoMarkerDetection.cpp
@@ -1,6 +1,19 @@
 #include "arUcoMarkerDetection.h"
-# include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/aruco.hpp>
 
 int startDetection() {
-	// drawMarker()
+    // Load the dictionary that was used to generate the markers.
+    Ptr<Dictionary> dictionary = getPredefinedDictionary(DICT_6X6_250);
+
+    // Initialize the detector parameters using default values
+    Ptr<DetectorParameters> parameters = DetectorParameters::create();
+
+    // Declare the vectors that would contain the detected marker corners and the rejected marker candidates
+    vector<vector<Point2f>> markerCorners, rejectedCandidates;
+
+    // The ids of the detected markers are stored in a vector
+    vector<int> markerIds;
+
+    // Detect the markers in the image
+    detectMarkers(frame, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
 }
